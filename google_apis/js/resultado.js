8491b251a22afd38530d05f48f757367a2254bf3
maldad <canond_7@hotmail.com>
2017-03-15 09:36:12 -0600

    extrayendo los datos necesarios desde un json de forma local

---
+++ google_apis/js/resultado.js
@@ -0,0 +1,8 @@
+var tipo = test.itemListElement[0].result.description;
+var descripcion = test.itemListElement[0].result.detailedDescription.articleBody;
+var image = test.itemListElement[0].result.image.url;
+var score = test.itemListElement[0].resultScore;
+console.log(tipo);
+console.log(descripcion);
+console.log(image);
+console.log(score);
